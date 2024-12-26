e2460d11aabed40b7f470dab9901f0f95ddd88dc
tqt97 <kutuanonline199@gmail.com>
2024-12-26 21:09:43 +0700

    Setup React Typescript with Vite & ESLint

--- Frontend/eslint.config.js
+++ Frontend/eslint.config.js
@@ -1,3 +1,4 @@
+import eslintPluginPrettier from 'eslint-plugin-prettier'
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
@@ -5,7 +6,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'vite.config.ts'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
@@ -16,6 +17,7 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier: eslintPluginPrettier
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +25,23 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      "react/react-in-jsx-scope": "off", //In React - 17.0.0, importing react to a file is optional, so it doesn't need to be imported
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+      'prettier/prettier': [
+        'warn',
+        {
+          arrowParens: 'always',
+          semi: false,
+          trailingComma: 'none',
+          tabWidth: 2,
+          endOfLine: 'auto',
+          useTabs: false,
+          singleQuote: true,
+          printWidth: 120,
+          jsxSingleQuote: true
+        }
+      ]
     },
   },
 )
